b8d0ef7022541999c2267fc7937f2a60e0da602c
Zack Hobson <zack@zackhobson.com>
2013-12-22 22:09:45 -0800

    net: cleanup

--- lib/hcl/net.rb
+++ lib/hcl/net.rb
@@ -2,54 +2,47 @@ require 'faraday_middleware'
 
 module HCl
   module Net
-    # configuration accessors
-    CONFIG_VARS = [ :login, :password, :subdomain, :ssl ].freeze
-    CONFIG_VARS.each do |config_var|
-      class_eval <<-EOC
-        def self.#{config_var}= arg
-          @@#{config_var} = arg
-        end
-        def self.#{config_var}
-          @@#{config_var}
-        end
-      EOC
-    end
+    class << self
+      # configuration accessors
+      CONFIG_VARS = [ :login, :password, :subdomain, :ssl ].freeze
+      CONFIG_VARS.each { |config_var| attr_accessor config_var }
 
-    def self.configure opts = nil
-      if opts
-        self.login = opts['login']
-        self.password = opts['password']
-        self.subdomain = opts['subdomain']
-        self.ssl = opts['ssl']
+      def config_hash
+        CONFIG_VARS.inject({}) {|c,k| c.update(k => send(k)) }
       end
-    end
 
-    def self.config_hash
-      CONFIG_VARS.inject({}) {|c,k| c.update(k => send(k)) }
-    end
-
-    def self.faraday
-      @faraday ||= Faraday.new(
-        "http#{ssl && 's'}://#{subdomain}.harvestapp.com"
-      ) do |f|
-        f.headers['Accept'] = 'application/json'
-        f.request :json
-        f.request :basic_auth, login, password
-        f.use HCl::HarvestMiddleware, content_type: /\bjson\b/
-        f.adapter Faraday.default_adapter
+      def configure opts = nil
+        if opts
+          self.login = opts['login']
+          self.password = opts['password']
+          self.subdomain = opts['subdomain']
+          self.ssl = opts['ssl']
+        end
       end
-    end
 
-    def self.get action
-      faraday.get(action).body
-    end
+      def faraday
+        @faraday ||= Faraday.new(
+          "http#{ssl && 's'}://#{subdomain}.harvestapp.com"
+        ) do |f|
+          f.headers['Accept'] = 'application/json'
+          f.request :json
+          f.request :basic_auth, login, password
+          f.use HCl::HarvestMiddleware, content_type: /\bjson\b/
+          f.adapter Faraday.default_adapter
+        end
+      end
 
-    def self.post action, data
-      faraday.post(action, data).body
-    end
+      def get action
+        faraday.get(action).body
+      end
 
-    def self.delete action
-      faraday.delete(action).body
+      def post action, data
+        faraday.post(action, data).body
+      end
+
+      def delete action
+        faraday.delete(action).body
+      end
     end
   end
 end
