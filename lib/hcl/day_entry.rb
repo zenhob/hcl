533d4cbe49321ef1090b0470eaf9914f810e74ea
Zack Hobson <zack@zackhobson.com>
2013-12-26 21:44:41 -0800

    day_entry: fix cancel

--- lib/hcl/day_entry.rb
+++ lib/hcl/day_entry.rb
@@ -17,7 +17,7 @@ module HCl
 
     def cancel
       begin
-        DayEntry.delete("daily/delete/#{id}")
+        Net.delete("daily/delete/#{id}")
       rescue HarvestMiddleware::Failure
         return false
       end
