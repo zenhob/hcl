09098fdcce8b6a32264cd680b24837d02a572ccb
Michael Clark <mclark@newvistas.com>
2018-06-19 13:41:56 -0600

    adding options, descriptions; have surpassed the original script (in dev time too no doubt...)

--- bin/hcl.fish
+++ bin/hcl.fish
@@ -1,13 +1,3 @@
-function __fish_hcl_using_command
-    set -l cmd (commandline -opc)
-    if [ (count $cmd) -gt 1 ]
-        if [ $argv[1] = $cmd[2] ]
-            return 0
-        end
-    end
-    return 1
-end
-
 function __fish_hcl_arg_count
     set -l cmd (commandline -opc)
     if [ (count $cmd) -eq $argv[1] ]
@@ -27,13 +17,34 @@ function __fish_hcl_tasks
 end
 
 function __fish_hcl_aliases
-    hcl aliases | sed -e "s/, /\n/g"
+    hcl aliases | sed -e 's/, /\n/g'
 end
 
 complete -c hcl -f
-complete -c hcl -n '__fish_hcl_arg_count 1' -f -a "start resume log stop note show tasks alias unalias aliases cancel nvm oops config status"
-complete -c hcl -n '__fish_hcl_using_command start; and __fish_hcl_arg_count 2' -f -a '(__fish_hcl_aliases)'
-complete -c hcl -n '__fish_hcl_using_command resume; and __fish_hcl_arg_count 2' -f -a '(__fish_hcl_aliases)'
-complete -c hcl -n '__fish_hcl_using_command log; and __fish_hcl_arg_count 2' -f -a '(__fish_hcl_aliases)'
-complete -c hcl -n '__fish_hcl_using_command alias; and __fish_hcl_arg_count 3' -a '(__fish_hcl_customers)'
-complete -c hcl -n '__fish_hcl_using_command alias; and __fish_hcl_arg_count 4' -a '(__fish_hcl_tasks)'
+
+complete -c hcl -f -s r -l reauth -d 'Force refresh of auth details'
+complete -c hcl -f -s c -l changelog -d 'Review the HCl changelog'
+complete -c hcl -f -s v -l version -d 'Print version and exit'
+complete -c hcl -f -s h -l help -d 'Show the help message'
+
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a tasks -d 'show all available tasks'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a alias -d 'create a task alias'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a unalias -d 'remove a task alias'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a aliases -d 'list task aliases'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a start -d 'start a task using an alias'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a stop -d 'stop a running timer'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a note -d 'add a line to a running timer'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a log -d 'log a task and time without leaving a timer running'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a resume -d 'resume the last stopped timer or a specific task'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a 'cancel nvm oops' -d 'delete the current or last running timer'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a show -d 'display the daily timesheet'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a config -d 'show your current credentials'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a status -d 'display Harvest service status'
+complete -c hcl -n '__fish_hcl_arg_count 1' -f -a '(__fish_hcl_aliases)' -d 'start work on aliased task'
+
+complete -c hcl -n '__fish_seen_subcommand_from start; and __fish_hcl_arg_count 2' -f -a '(__fish_hcl_aliases)'
+complete -c hcl -n '__fish_seen_subcommand_from resume; and __fish_hcl_arg_count 2' -f -a '(__fish_hcl_aliases)'
+complete -c hcl -n '__fish_seen_subcommand_from log; and __fish_hcl_arg_count 2' -f -a '(__fish_hcl_aliases)'
+
+complete -c hcl -n '__fish_seen_subcommand_from alias; and __fish_hcl_arg_count 3' -f -a '(__fish_hcl_customers)'
+complete -c hcl -n '__fish_seen_subcommand_from alias; and __fish_hcl_arg_count 4' -f -a '(__fish_hcl_tasks)'
